21510ec284c601778ebb056f6e73c181c047c8c1
Dyllan Macias <DellanX@Live.com>
2025-11-14 08:20:43 -0700

    fix: convert rollup configto ESM

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,20 @@
-const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const typescript = require('rollup-plugin-typescript2');
-const pkg = require('./package.json');
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+// Read package.json without requiring JSON import assertions
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = __filename.substring(0, __filename.lastIndexOf('/'));
+const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
 
 const external = [
   ...Object.keys(pkg.peerDependencies || {}),
   ...Object.keys(pkg.dependencies || {})
 ];
 
-module.exports = {
+export default {
   input: 'source/index.ts',
   external,
   plugins: [
